230eda77a6fbed2ef6b714ffb73b5d50745c7e49
Mattia Bondrano <mattcage89@gmail.com>
2015-07-20 21:19:23 +0200

    Animazione

--- js/main.js
+++ js/main.js
@@ -1,4 +1,16 @@
-$(document).ready(function(){	//executed after the page has loaded
+$(window).load(function () {
+	$('body').sakura({
+		blowAnimations: ['blow-medium-right', 'blow-hard-right'], // Horizontal movement animation names
+		className: 'sakura', // Class name to use
+		fallSpeed: 2, // Factor for petal fall speed
+		maxSize: 60, // Maximum petal size
+		minSize: 50, // Minimum petal size
+		newOn: 600, // Interval after which a new petal is added
+		swayAnimations: ['sway-0', 'sway-1', 'sway-2', 'sway-3', 'sway-4', 'sway-5', 'sway-6', 'sway-7', 'sway-8'] // Swaying animation names
+	});
+});
+
+/*$(document).ready(function(){	//executed after the page has loaded
 
     checkURL();	//check if the URL has a reference to a page and load it
 
@@ -35,6 +47,7 @@ $(document).ready(function(){	//executed after the page has loaded
 
 });
 
+
 var lasturl="";	//here we store the current URL hash
 
 function checkURL(hash)
@@ -70,4 +83,4 @@ function loadPage(url)	//the function that loads pages via AJAX
 
     });
 
-}
+}*/
