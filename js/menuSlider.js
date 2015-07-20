6148ea6754ce8454c9e44cd53804b081e103155e
Mattia Bondrano <mattcage89@gmail.com>
2015-07-21 01:41:28 +0200

    Fix Finale 5

--- js/menuSlider.js
+++ js/menuSlider.js
@@ -1,7 +1,12 @@
+// JS che gestisce il menu
+
+// Se la finestra è < di 992px, nascondi il menu
 if( $( window ).width() < 992 ) $( '#menu-container' ).hide();
-	
+
+// Al click su #bottoneLogo
 $( '#bottoneLogo' ).click( function() {
 	
+	// Se la finestra è < di 992px toggla il menu, altrimenti non intervenire
 	if( $( window ).width() < 992 ) $( '#menu-container' ).slideToggle();
 		
 });
